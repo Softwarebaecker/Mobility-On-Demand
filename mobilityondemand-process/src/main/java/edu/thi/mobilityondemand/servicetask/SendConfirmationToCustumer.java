94176c05b84df45985464d2c33cb9a02fe9cd312
adnors <sandro.kaeppner@gmail.com>
2019-12-03 13:15:14 +0100

    Add classes to make process compileable

---
+++ mobilityondemand-process/src/main/java/edu/thi/mobilityondemand/servicetask/SendConfirmationToCustumer.java
@@ -0,0 +1,11 @@
+package edu.thi.mobilityondemand.servicetask;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class SendConfirmationToCustumer implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+    }
+}
