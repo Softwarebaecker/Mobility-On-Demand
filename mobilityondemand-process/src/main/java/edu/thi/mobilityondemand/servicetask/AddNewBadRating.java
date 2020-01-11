e9e2f023d62c10326b442cc2a7512ef54cafd26e
adnors <sandro.kaeppner@gmail.com>
2020-01-11 11:35:49 +0100

    Add classes to rating process

---
+++ mobilityondemand-process/src/main/java/edu/thi/mobilityondemand/servicetask/AddNewBadRating.java
@@ -0,0 +1,11 @@
+package edu.thi.mobilityondemand.servicetask;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class AddNewBadRating implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+    }
+}
