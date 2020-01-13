0d7fbad5ad42bd864567e27b09188092ebdd4e87
Softwarebaecker <nil.kuchenbaecker@gmail.com>
2020-01-13 14:21:31 +0100

    Fix WeatherToCamunda

--- mobilityondemand-camel/src/main/java/edu/thi/mobilityondemand/camel/routes/RouteBuilderWeatherToCamunda.java
+++ mobilityondemand-camel/src/main/java/edu/thi/mobilityondemand/camel/routes/RouteBuilderWeatherToCamunda.java
@@ -11,7 +11,7 @@ public class RouteBuilderWeatherToCamunda extends RouteBuilder {
         restConfiguration().host("localhost:8080/engine-rest");
 
         Endpoint source = endpoint("jms:topic:StormyWeatherEventTopic");
-        Endpoint destination = endpoint("http://localhost:8080/engine-rest/message");
+        Endpoint destination = endpoint("http://localhost:8080/engine-rest/signal");
 
         /**
          * Taxi send direct JSON :D
@@ -24,10 +24,10 @@ public class RouteBuilderWeatherToCamunda extends RouteBuilder {
             .process(new Processor() {
                 @Override
                 public void process(Exchange exchange) throws Exception {
-                    String body = "{ \"messageName\" : \"weather_alert\" }";
-                    exchange.getIn().setHeader(Exchange.CONTENT_TYPE, constant("application/json"));
-                    exchange.getIn().setHeader(Exchange.HTTP_METHOD, simple("POST"));
-                    exchange.getIn().setBody(body);
+                    String body = "{ \"name\" : \"weather_alert\" }";
+                    exchange.getOut().setHeader(Exchange.CONTENT_TYPE, "application/json");
+                    exchange.getOut().setHeader(Exchange.HTTP_METHOD, "POST");
+                    exchange.getOut().setBody(body);
                 }
             })
             .log("new Weather Alert")
