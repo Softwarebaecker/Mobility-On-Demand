cad5580fca57163e9f1baa5b183dfed063abf052
adnors <sandro.kaeppner@gmail.com>
2020-01-13 14:45:00 +0100

    Implement SendCancellationToCustomer

--- mobilityondemand-process/src/main/java/edu/thi/mobilityondemand/servicetask/SendCancellationToCustomer.java
+++ mobilityondemand-process/src/main/java/edu/thi/mobilityondemand/servicetask/SendCancellationToCustomer.java
@@ -1,11 +1,23 @@
+// Sandro Käppner
+
 package edu.thi.mobilityondemand.servicetask;
 
+import edu.thi.mobilityondemand.process.message.CancellationMessage;
+import edu.thi.mobilityondemand.process.queue.MessageQueue;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+import javax.jms.Message;
+
 public class SendCancellationToCustomer implements JavaDelegate {
     @Override
-    public void execute(DelegateExecution delegateExecution) throws Exception {
-        //TODO: Send Message to Customer: "We can't serve this Route!"
+    public void execute(DelegateExecution execution) throws Exception {
+        Long customerId = (Long) execution.getVariable("customerId");
+
+        CancellationMessage messageObject = new CancellationMessage(customerId);
+
+        MessageQueue mq = new MessageQueue();
+        Message message = mq.createSession().createTextMessage(messageObject.toXml());
+        mq.sendMessageToQueue("toCustomer", message);
     }
 }
