d82ae9f8900f279016460836d07221b401b0897c
Softwarebaecker <nil.kuchenbaecker@gmail.com>
2020-01-16 16:13:13 +0100

    Remove get Taxi ID from Process & formate Camel

--- mobilityondemand-camel/src/main/java/edu/thi/mobilityondemand/camel/routes/RouteBuilderCamundaToCustomer.java
+++ mobilityondemand-camel/src/main/java/edu/thi/mobilityondemand/camel/routes/RouteBuilderCamundaToCustomer.java
@@ -15,21 +15,21 @@ public class RouteBuilderCamundaToCustomer extends RouteBuilder {
         from(source)
                 .setHeader("customerId",xpath("/*/customerId/text()", String.class))
                 .choice().when(xpath("/tripDataMessageToCustomer"))
-                .unmarshal().jacksonxml()     //convert from xml to Java Object
-                .marshal(json)
-                .log("New Trip Confirmation Message to Customer ${header.customerId}")
-                .to("file:./CamundaToCustomersMessages?fileName=TripConfirmation_${date:now:yyyy-MM-dd_HH-mm-ss-SS}_Customer_${header.customerId}")
+                    .unmarshal().jacksonxml()     //convert from xml to Java Object
+                    .marshal(json)
+                    .log("New Trip Confirmation Message to Customer ${header.customerId}")
+                    .to("file:./CamundaToCustomersMessages?fileName=TripConfirmation_${date:now:yyyy-MM-dd_HH-mm-ss-SS}_Customer_${header.customerId}")
                 .when(xpath("/cancellationMessage"))
-                .unmarshal().jacksonxml()     //convert from xml to Java Object
-                .marshal(json)
-                .log("New Cancellation Message to Customer ${header.customerId}")
-                .to("file:./CamundaToCustomersMessages?fileName=CancellationMessage${date:now:yyyy-MM-dd_HH-mm-ss-SS}_Customer_${header.customerId}")
+                    .unmarshal().jacksonxml()     //convert from xml to Java Object
+                    .marshal(json)
+                    .log("New Cancellation Message to Customer ${header.customerId}")
+                    .to("file:./CamundaToCustomersMessages?fileName=CancellationMessage${date:now:yyyy-MM-dd_HH-mm-ss-SS}_Customer_${header.customerId}")
                 .when(xpath("/ratingRequestMessage"))
-                .unmarshal().jacksonxml()     //convert from xml to Java Object
-                .marshal(json)
-                .log("New Rating Request to Customer ${header.customerId}")
-                .to("file:./CamundaToCustomersMessages?fileName=RatingRequestMessage${date:now:yyyy-MM-dd_HH-mm-ss-SS}_Customer_${header.customerId}")
+                    .unmarshal().jacksonxml()     //convert from xml to Java Object
+                    .marshal(json)
+                    .log("New Rating Request to Customer ${header.customerId}")
+                    .to("file:./CamundaToCustomersMessages?fileName=RatingRequestMessage${date:now:yyyy-MM-dd_HH-mm-ss-SS}_Customer_${header.customerId}")
                 .otherwise()
-                .log("unknown Message: ${body}");
+                    .log("unknown Message: ${body}");
     }
 }
